48089dc45b1d8cf66bf1ce4bf3abcae96c88008c
KOUSSEMONAUREL <koussemonaurel@gmail.com>
2025-06-18 09:58:39 +0100

    Update settings.py

--- PIL1_2425_10/PIL1_2425_10/settings.py
+++ PIL1_2425_10/PIL1_2425_10/settings.py
@@ -140,14 +140,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-STATIC_URL = '/static/'
-DATABASES={
-    'default':{
-        'ENGINE':
-        'django.db.backends.sqlite3', 'NAME':BASE_DIR/ 'db.sqlite3',
 
-    }
-}
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'IFRI_comotorage', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
